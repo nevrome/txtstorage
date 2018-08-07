8b0291b6629e287cf706123b5ab694f3ca9bd2b9
nevrome <clemens@nevrome.de>
2018-08-08 00:46:31 +0200

    started to work on a very, very simple information storage system

---
+++ R/information_storage.R
@@ -0,0 +1,31 @@
+#' @export
+store_info <- function(name, value, path) {
+  create_storage_if_not_exists(path)
+  storage <- read_storage(path)
+  if (any(storage$name == name)) { 
+    storage$value[storage$name == name] <- value 
+  } else {
+    storage <- rbind(storage, data.frame(name, value))
+  }
+  write.csv(storage, file = path)
+}
+
+#' @export
+read_info <- function(name, path) {
+  storage <- read_storage(path)
+  return(storage$value[storage$name == name])
+}
+
+read_storage <- function(path) {
+  read.csv(path, stringsAsFactors = F)
+}
+
+# file system interaction
+create_storage <- function(path) {
+  write.csv(
+    data.frame(name = character(), value = character(), stringsAsFactors = F), 
+    file = path
+  )
+}
+check_if_storage_exists <- function(path) file.exists(path)
+create_storage_if_not_exists <- function(path) if (!check_if_storage_exists(path)) create_storage(path)
